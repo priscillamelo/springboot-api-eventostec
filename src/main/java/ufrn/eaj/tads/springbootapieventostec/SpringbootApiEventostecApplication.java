62a157046915739743ce959b563ef94cfc475407
priscillamelo <priscillamelo73@gmail.com>
2024-09-18 18:25:58 -0300

    feat: first commit

---
+++ src/main/java/ufrn/eaj/tads/springbootapieventostec/SpringbootApiEventostecApplication.java
@@ -0,0 +1,13 @@
+package ufrn.eaj.tads.springbootapieventostec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiEventostecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApiEventostecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ufrn/eaj/tads/springbootapieventostec/SpringbootApiEventostecApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.eaj.tads.springbootapieventostec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiEventostecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
